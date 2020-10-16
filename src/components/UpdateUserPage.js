18c722f147161d68c8f042f2d091eade90091e89
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-10-16 17:32:34 -0400

    fixed up layout due to a misunderstanding in functionality of update method

--- src/components/UpdateUserPage.js
+++ src/components/UpdateUserPage.js
@@ -1,27 +1,19 @@
-import React, { Component, useRef } from "react";
+import React, { Component } from "react";
 import "./UpdateUserPage.css";
 
 export class UpdateUserPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { newUsername: "", newPassword: "", isUpdatingPassword: false };
+    this.state = {
+      newUsername: "",
+      newPassword: "",
+    };
 
     //functions
     this.updatePassword = this.updatePassword.bind(this);
     this.updateUsername = this.updateUsername.bind(this);
-    this.setToUpdatePassword = this.setToUpdatePassword.bind(this);
-    this.setToUpdateUsername = this.setToUpdateUsername.bind(this);
-    this.handleSubmitPassword = this.handleSubmitPassword.bind(this);
-    this.handleSubmitUsername = this.handleSubmitUsername.bind(this);
-    this.handleReturn = this.handleReturn.bind(this)
-  }
-
-  setToUpdatePassword() {
-      this.setState({isUpdatingPassword: true})
-  }
-
-  setToUpdateUsername() {
-      this.setState({isUpdatingPassword: false})
+    this.handleSubmitUsername = this.handleSubmit.bind(this);
+    this.handleReturn = this.handleReturn.bind(this);
   }
 
   updatePassword(event) {
@@ -32,11 +24,7 @@ export class UpdateUserPage extends Component {
     this.setState({ newUsername: event.target.value });
   }
 
-  handleSubmitPassword() {
-    //TODO perform put API call
-  }
-
-  handleSubmitUsername() {
+  handleSubmit() {
     //TODO perform put API call
   }
 
@@ -47,46 +35,28 @@ export class UpdateUserPage extends Component {
   render() {
     return (
       <div id="container" class="container">
-        <button class="returnButton" onClick={this.handleReturn}>Return</button>
+        <button class="returnButton" onClick={this.handleReturn}>
+          Return
+        </button>
         <h1> Update User Information</h1>
-        <div class="buttonWrapper">
-          <button class="passwordButton" onClick={this.setToUpdatePassword}>
-            Change Password
-          </button>
-          <button class="usernameButton" onClick={this.setToUpdateUsername}>
-            Change Username
-          </button>
-        </div>
         <div class="fieldContainer">
-          <div class={this.state.isUpdatingPassword ? "changePasswordContainer": 'hide'}>
-            <label>Old Password</label>
-            <input type="password" id="oldPasswordInput"></input>
-            <label>New Password</label>
-            <input
-              type="text"
-              id="newPasswordInput"
-              value={this.state.newPassword}
-              onChange={this.updatePassword}
-            ></input>
-            <button id="passwordUpdateButton" class="updateButton">
-              Update
-            </button>
-          </div>
-
-          <div class={this.state.isUpdatingPassword ? "hide": "changeUsernameContainer"}>
-            <label>New Username</label>
-            <input
-              type="text"
-              id="newUsernameInput"
-              value={this.state.Username}
-              onChange={this.updateUsername}
-            ></input>
-            <label>Password</label>
-            <input type="password" id="passwordInput"></input>
-            <button id="usernameUpdateButton" class="updateButton">
-              Update
-            </button>
-          </div>
+        <label>New Username</label>
+          <input
+            type="text"
+            id="newUsernameInput"
+            value={this.state.Username}
+            onChange={this.updateUsername}
+          ></input>
+          <label>New Password</label>
+          <input
+            type="text"
+            id="newPasswordInput"
+            value={this.state.newPassword}
+            onChange={this.updatePassword}
+          ></input>
+          <button class="updateButton" onClick={this.handleSubmit}>
+            Update
+          </button>
         </div>
       </div>
     );
