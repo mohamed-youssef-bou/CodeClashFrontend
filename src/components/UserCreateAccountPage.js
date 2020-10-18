cbf9d4dd9b988853de0bffc8a06f346c6148bc60
Mairead Maloney <mairead.maloney@mail.mcgill.ca>
2020-10-18 12:53:25 -0400

    login navigation done

---
+++ src/components/UserCreateAccountPage.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Redirect } from 'react-router-dom';
+
+
+export class UserCreateAccountPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state= {username: '', email: '', password: '', navReady: false};
+    
+        this.updateUsername = this.updateUsername.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+     }
+
+    updateUsername(event){
+        this.setState({username: event.target.value});
+    }
+    updateEmail(event){
+        this.setState({email: event.target.value});       
+    }
+    updatePassword(event){
+        this.setState({password: event.target.value});       
+    }
+
+
+
+        handleSubmit(event) {
+            event.preventDefault()
+            alert('Username is: ' + this.state.username + ' | Password is: ' + this.state.password);
+            let data = {
+                'username': this.state.username,
+                'email': this.state.email,
+                'password': this.state.password
+                  
+              };
+            console.log(JSON.stringify(data))
+            fetch(`http://localhost:9000/createUser`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }) // Login with body parameters: username and password
+            .then((res) => {
+                if(res.status === 201){
+                    this.setState({navReady: true})
+                    console.log(res.json());    
+                }})
+                .then(data => localStorage["token"] = data)
+                
+                // .then(this.setState({navReady: true}))
+                .catch(err => err);
+        }
+    
+
+
+    render() {
+        if (this.state.navReady) {
+            return <Redirect to='/landing_page' />
+          }
+        return (
+            <div id="loginForm">
+                <form class="form-inline" onSubmit={this.handleSubmit}>
+                    {/* Username */}
+                    <label class="sr-only" for="usernameInput">Username</label>
+                    <input type="name" class="form-control mb-2 mr-sm-2" id="usernameInput" placeholder="Username" value={this.state.username} onChange={this.updateUsername}></input>
+
+                    {/* Email */}
+                    <label class="sr-only" for="emailInput">Email</label>
+                    <div class="input-group mb-2 mr-sm-2">
+                        <input type="email" class="form-control" id="emailInput" placeholder="Email" value={this.state.email} onChange={this.updateEmail}></input>
+                    </div>
+
+                    {/* Password */}
+                    <label class="sr-only" for="passwordInput">Password</label>
+                    <div class="input-group mb-2 mr-sm-2">
+                        <input type="password" class="form-control" id="passwordInput" placeholder="Password" value={this.state.password} onChange={this.updatePassword}></input>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-2">Create Account</button>
+
+                </form>
+                {/* <button type="navigate" class="btn btn-primary mb-2" onPressed={() => this.navLandingPage}>Landing Page </button> */}
+
+            </div>
+
+        );
+    }
+}
+ 
+export default UserCreateAccountPage;
