f2debde19d06b6ae7a8b1310c1c20565ac96ab5e
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-10-18 17:16:57 -0400

    added navigation from Update to Landing and from Landing to Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import UserLoginPage from "./components/UserLoginPage";
+// import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+// import UserLandingPage from './components/UserLandingPage'
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { apiResponse: "" };
+//     }
+
+//     callAPI() {
+//         fetch("/test")
+//             .then(res => res.text())
+//             .then(res => this.setState({ apiResponse: res }))
+//             .catch(err => err);
+//     }
+
+//     componentDidMount() {
+//         this.callAPI();
+//     }
+
+//     render() {
+//         return (
+//             <div className="App">
+//                 <header className="App-header">
+//                     <img src={logo} className="App-logo" alt="logo" />
+//                     <h1 className="App-title">Welcome to React</h1>
+//                 </header>
+//                 <p className="App-intro">{this.state.apiResponse}</p>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
+
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import UserLoginPage from "./components/UserLoginPage";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import UserLandingPage from './components/UserLandingPage'
+import UserLandingPage from './components/UserLandingPage.js';
+import UserLoginPage from "./components/UserLoginPage";
+import UpdateUserPage from "./components/UpdateUserPage.js";
 
 class App extends Component {
     constructor(props) {
@@ -11,26 +51,29 @@ class App extends Component {
         this.state = { apiResponse: "" };
     }
 
-    callAPI() {
-        fetch("/test")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
-    }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+            <Router>
+            <div>
+              <h2>Welcome to React</h2>
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <ul className="navbar-nav mr-auto">
+                <li><Link to={'/'} className="nav-link"> Home </Link></li>
+                
+                <li><Link to={'/login'} className="nav-link">Login</Link></li>
+                <li><Link to={'/update'} className="nav-link">Update User Info</Link></li>
+              </ul>
+              </nav>
+              <hr />
+              <Switch>
+                  <Route path='/login' component={UserLoginPage} />                  
+                  <Route path='/landing_page' component={UserLandingPage} />
+                  <Route path='/update' component={UpdateUserPage} />
+              </Switch>
             </div>
+          </Router>
+            
         );
     }
 }
