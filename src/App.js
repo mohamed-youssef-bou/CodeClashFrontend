16228aa34d4968276ada3115f9b950b8c4f691f0
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-10-21 17:34:13 -0400

    Added button and navigation to CreateChallengePage from landingPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UserLandingPage from './components/UserLandingPage';
 import UserCreateAccountPage from "./components/UserCreateAccountPage";
 import UserLoginPage from "./components/UserLoginPage";
 import UpdateUserPage from "./components/UpdateUserPage";
+import CreateChallengePage from "./components/CreateChallengePage";
 
 class App extends Component {
     constructor(props) {
@@ -17,13 +18,14 @@ class App extends Component {
         return (
             <Router>
             <div>
-              <h2>Welcome to React</h2>
+              <h2 style={{textAlign: "center"}}>Welcome to PuzzlR</h2>
               <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <ul className="navbar-nav mr-auto">
                 <li><Link to={'/'} className="nav-link"> Home </Link></li>         
                 <li><Link to={'/login'} className="nav-link">Login</Link></li>
                 <li><Link to={'/create_account'} className="nav-link">Create Account</Link></li>
                 <li><Link to={'/update'} className="nav-link">Update User Info</Link></li>
+                <li><Link to={'/create_challenge'} className="nav-link">Create Challenge</Link></li>
               </ul>
               </nav>
               <hr />
@@ -31,7 +33,8 @@ class App extends Component {
                   <Route path='/login' component={UserLoginPage} />                  
                   <Route path='/landing_page' component={UserLandingPage} />
                   <Route path='/update' component={UpdateUserPage} />
-                  <Route path='/create_account' component={UserCreateAccountPage} /> 
+                  <Route path='/create_account' component={UserCreateAccountPage} />
+                  <Route path='/create_challenge' component={CreateChallengePage}></Route>
               </Switch>
             </div>
           </Router>
---
+++ src/components/CreateChallengePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class CreateChallengePage extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        
+      };
+
+    }
+
+    render () {
+      return (
+        <div>
+          Add Create Challenge input fields
+        </div>
+      )
+    }
+}
+export default CreateChallengePage;
