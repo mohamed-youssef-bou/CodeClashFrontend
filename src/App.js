8a9e68346d95de8fa0a59a1caf2901a7bf4316e2
Mairead Maloney <mairead.maloney@mail.mcgill.ca>
2020-10-29 14:17:31 -0400

    Base Page organization done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import button from 'react-bootstrap/Button';
 import "./App.css";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import UserLandingPage from './components/UserLandingPage';
+import { BrowserRouter as Router, Link, Switch, Route, Redirect } from "react-router-dom";
 import UserCreateAccountPage from "./components/UserCreateAccountPage";
 import UserLoginPage from "./components/UserLoginPage";
-import UpdateUserPage from "./components/UpdateUserPage";
-import CreateChallengePage from "./components/CreateChallengePage";
-import logo from "./assets/logo.png"
+import LinkButton from "./components/LinkButton"
 
 class App extends Component {
     constructor(props) {
@@ -20,25 +16,54 @@ class App extends Component {
 
     render() {
         return (
+            <Router>
+                <Switch>
+                <Route path="/" exact render={() => {
+                    return (
+                      <div class="window">
+                      <section class="container">
+                      <div class="left-half">
+                      <img class="logo" src={require('/Users/maireadmaloney/Documents/ECSE428/ECSE428_G07_Frontend/src/assets/logoEdited.png')}>
+                      </img>
+                      <div>
+                          <LinkButton to='/login'>Login</LinkButton>
+                          <LinkButton to='/create_account'>Sign Up</LinkButton>          
+                    
+                      </div>
+                      
+                    </div>
+                    <div class="right-half">
+                    </div>
+                    
+                    </section>
+                    </div>
+                    );
+                }}/>
+                     <Route path='/login' component={UserLoginPage} />                  
+                     
+                     <Route path='/create_account' component={UserCreateAccountPage} />                
+                     </Switch>
+            </Router>
+       
+  
+        // return (
+        //   <Router>
+            
+        // <Switch>
+        // <Route path="/" exact render={() => {
+        //             return (
+                      
+        //               );
+        //             }}/>
+        //            <Route path='/login' component={UserLoginPage} />                  
+        //            <Route path='/create_account' component={UserCreateAccountPage} />
+        //     </Switch>
+        //     </div>
+        // </Router>
 
-          <section class="container">
-          <div class="left-half">
-            <img class="logo" src={require('/Users/maireadmaloney/Documents/ECSE428/ECSE428_G07_Frontend/src/assets/logoEdited.png')}>
-            </img>
-            <div>
-              <div>
-                <button class="btn-style"> Login</button>
-                <button class="btn-style">Sign Up</button>
-              </div>
-                          
-            </div>
-          </div>
-          <div class="right-half">
-          </div>
-        </section>
-          
         
           
+      
           //   <Router>
           //   <div>
           //     <h2 style={{textAlign: "center"}}>Welcome to PuzzlR</h2>
