f74a40aa7f2eea1cc990dc4b0f8630964fa05297
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-10-29 17:35:15 -0400

    preliminary edits complete

--- src/components/UpdateUserPage.js
+++ src/components/UpdateUserPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./UpdateUserPage.css";
-import { Redirect } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
 export class UpdateUserPage extends Component {
@@ -30,58 +30,64 @@ export class UpdateUserPage extends Component {
     event.preventDefault();
 
     let data = {
-      'user_id': jwt_decode(localStorage.getItem("token")).user._id,
-      'new_username': this.state.newUsername,
-      'new_password': this.state.newPassword,
+      user_id: jwt_decode(localStorage.getItem("token")).user._id,
+      new_username: this.state.newUsername,
+      new_password: this.state.newPassword,
     };
 
     fetch("/updateUser", {
       method: "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
     //set error text to display
   }
 
   handleReturn() {
     //use Redirect from react-router-dom
     this.setState({ isRedirect: true });
-
   }
 
   render() {
-    if( this.state.isRedirect) {
-      return <Redirect to="/landing_page" />
+    if (this.state.isRedirect) {
+      return <Redirect to="/landing_page" />;
       //NEED TO ADD PAGE IN THE ROUTES OF APP.JS
     }
 
     return (
       <div id="container" class="container">
-        <button class="returnButton" onClick={this.handleReturn}>
-          Return
-        </button>
-        <h1> Update User Information</h1>
         <div class="fieldContainer">
-        <label>New Username</label>
+          <div class="logo"> insert logo here</div>
+          <h1> Update Account Details</h1>
+          <label>New Username</label>
           <input
             type="text"
             value={this.state.newUsername}
             onChange={this.updateUsername}
           ></input>
+          <label>Email Address</label>
+          <div class="emailDiv">get value of user's email here</div>
           <label>New Password</label>
           <input
             type="text"
             value={this.state.newPassword}
             onChange={this.updatePassword}
           ></input>
+          <div class="buttonContainer">
           <button class="updateButton" onClick={this.handleSubmit}>
             Update
           </button>
+          <button class="returnButton" onClick={this.handleReturn}>
+            Return
+          </button>
+          </div>
           <p class="errorMessage"></p>
+        </div>
+        <div class="rightBackgroundContainer">
         </div>
       </div>
     );
