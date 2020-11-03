2838502c0b94951ff80f9e6516136578286af9fc
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-11-03 18:14:57 -0500

    initial design of ListAllChallengesPage

--- src/components/ListAllChallengesPage.js
+++ src/components/ListAllChallengesPage.js
@@ -1,20 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./ListAllChallengesPage.css";
 
 export class ListAllChallengesPage extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      challenges: [],
+    };
+  }
 
-    }
-
-    render () {
-      return (
-        <div>
-          Add list of challenges here!
-        </div>
+  callAPI() {
+    fetch("http://localhost:9000/") //insert correct link
+      .then((res) => res.json())
+      .then((res) =>
+        this.setState({
+          challenges: res.challenges,
+        })
       )
-    }
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div class="challengesPageContainer">
+        <div class="logoChallengesPage"></div>
+        <div class="challengesPageSubcontainer">
+          <h1 class="title">Available Challenges</h1>
+          <div class="challengesList">
+            {this.state.challenges.map((challenge) => (
+              <li>{challenge.name}</li>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default ListAllChallengesPage;
