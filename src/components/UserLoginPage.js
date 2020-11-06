0fcc7fbf8ca7564af8da0f4861f6e73e610e1d1f
Christina <christina.riachi@mail.mcgill.ca>
2020-11-05 23:17:15 -0500

    UI of login page, logo img cut off by login form and header

--- src/components/UserLoginPage.js
+++ src/components/UserLoginPage.js
@@ -1,80 +1,127 @@
 import React, { Component } from "react";
-import 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Redirect } from 'react-router-dom';
-
+import "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/UserLoginPage.css";
+import { Redirect } from "react-router-dom";
 export class UserLoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      navReady: false,
+      returnToBase: false,
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {username: '', password: '', navReady: false};
+    this.updateUsername = this.updateUsername.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.handleReturn = this.handleReturn.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        this.updateUsername = this.updateUsername.bind(this);
-        this.updatePassword = this.updatePassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  updateUsername(event) {
+    this.setState({ username: event.target.value });
+  }
 
-    updateUsername(event){
-        this.setState({username: event.target.value});
-    }
+  updatePassword(event) {
+    this.setState({ password: event.target.value });
+  }
 
-    updatePassword(event){
-        this.setState({password: event.target.value});
-    }
+  handleReturn() {
+    this.setState({ returnToBase: true });
+  }
 
-    async handleSubmit(event) {
+  async handleSubmit(event) {
+    event.preventDefault();
 
-        event.preventDefault();
+    var isRedirect = false;
 
-        var isRedirect = false;
-
-        let data = {
-            'username': this.state.username,
-            'password': this.state.password
-          };
-          console.log(JSON.stringify(data));
-        await fetch('/login',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }) // Login with body parameters: username and password
-            .then((res) => {
-                if(res.status === 200){
-                    isRedirect = true;  
-                }
-                return res.json();
-        })
-        .then(data => localStorage["token"] = data["token"])
-        .catch(err => console.log(err));
-        
-        this.setState({navReady: true}) 
-    }
-
-    render() {
-        if (this.state.navReady) {
-            return <Redirect to='/landing_page' />
+    let data = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    console.log(JSON.stringify(data));
+    await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }) // Login with body parameters: username and password
+      .then((res) => {
+        if (res.status === 200) {
+          isRedirect = true;
         }
-        return (
-            <div id="loginForm">
-                <form className="form-inline" onSubmit={this.handleSubmit}>
-                    {/* Username */}
-                    <label className="sr-only" htmlFor="usernameInput">Username</label>
-                    <input type="name" className="form-control mb-2 mr-sm-2" id="usernameInput" placeholder="Username" value={this.state.username} onChange={this.updateUsername}></input>
+        return res.json();
+      })
+      .then((data) => (localStorage["token"] = data["token"]))
+      .catch((err) => console.log(err));
 
-                    {/* Password */}
-                    <label className="sr-only" htmlFor="passwordInput">Password</label>
-                    <div className="input-group mb-2 mr-sm-2">
-                        <input type="password" className="form-control" id="passwordInput" placeholder="Password" value={this.state.password} onChange={this.updatePassword}></input>
-                    </div>
+    this.setState({ navReady: true });
+  }
 
-                    <button type="submit" className="btn btn-primary mb-2">Login</button>
-                </form>
-            </div>
-        );
+  render() {
+    if (this.state.navReady) {
+      return <Redirect to="/landing_page" />;
     }
+    if (this.state.returnToBase) {
+      return <Redirect to="/" />;
+    }
+    const leftArrow = require("../assets/leftArrow.png");
+    const rightArrow = require("../assets/rightArrow.png");
+    return (
+      <div id="loginForm">
+        <div class="leftContainer">
+          <div class="logo" />
+          <h1>Login</h1>
+          <form
+            className="form-inline"
+            class="loginForm"
+            onSubmit={this.handleSubmit}
+          >
+            {/* Username */}
+            <label class="loginLabels" htmlFor="usernameInput">
+              Username
+            </label>
+            <input
+              type="name"
+              className="form-control"
+              id="usernameInput"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.updateUsername}
+            ></input>
+
+            {/* Password */}
+            <label class="loginLabels" htmlFor="passwordInput">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="passwordInput"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.updatePassword}
+            ></input>
+          </form>
+          <div className="loginButtons">
+            <button type="submit" className="loginSubmit">
+              <img className="left arrow" src={leftArrow} />
+              Login
+              <img className="right arrow" src={rightArrow} />
+            </button>
+            <button className="loginReturn" onClick={this.handleReturn}>
+              <img className="left arrow" src={leftArrow} />
+              Return
+              <img className="right arrow" src={rightArrow} />
+            </button>
+          </div>
+        </div>
+        <div class="rightContainer" />
+      </div>
+    );
+  }
 }
 
 export default UserLoginPage;
